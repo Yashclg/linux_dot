86a5f3cfd17c8091ef1e6fc9722b887cea3782bd
SDK <sadiksaifi205@gmail.com>
2022-02-26 14:34:56 +0530

    misc

--- .config/dmenu/config.def.h
+++ .config/dmenu/config.def.h
@@ -33,7 +33,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 30;  /* -h option; minimum height of a menu line */
+static unsigned int lineheight = 32;  /* -h option; minimum height of a menu line */
 static unsigned int columns    = 1;
 
 /*
