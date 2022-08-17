5cd2878cd8b73d637d91451ca018efff92765591
SDK <sadiksaifi205@gmail.com>
2022-08-17 22:42:13 +0530

    misc

---
+++ misc/packages.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+paru -S bluez bluez-utils xorg linux-headers linux linux-firmware base-devel networkmanager grub bspwm polybar sxhkd montserrat-otf ttf-nerd-fonts-hack-complete-git udiskie udisks2 alacritty dunst feh picom-ibhagwan-git xorg-xsetroot xorg-xbacklight xbanish pamixer neofetch tty-clock lf-bin htop exa gpick nitrogen flameshot tdrop wmctrl xdotool libinput-gestures ueberzug
