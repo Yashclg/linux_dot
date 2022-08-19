a3004124de90931bd18308770ee279e62c8364d3
SDK <sadiksaifi205@gmail.com>
2022-08-19 17:08:57 +0530

    misc

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#Killall Programs
+killall -9 polybar sxhkd dunst nitrogen udiskie picom #nm-applet blueman-applet pasystray
+
+#Polybar
+exec $HOME/.config/bspwm/scripts/launchBar.sh &
+
+#Start Hotkey Deamon
+sxhkd -c $HOME/.config/bspwm/sxhkdrc &
+
+#Compositor
+picom --config $HOME/.config/bspwm/picom.conf --experimental-backends &
+
+#Set Wallpaper
+exec $HOME/.config/bspwm/scripts/background.sh &
+#nitrogen --restore
+
+#Fix Cursor
+xsetroot -cursor_name left_ptr &
+
+#Notification
+dunst -config $HOME/.config/bspwm/dunstrc &
+
+#Automounting
+udiskie &
+
+#For Hiding Cursor while typing
+xbanish &
+
+# For System Tray
+# nm-applet &
+# blueman-applet &
+# pasystray &
